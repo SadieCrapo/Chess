b27cb3f13a1775d0fc93156c45d590ceca606700
SadieCrapo <scrapo5@byu.edu>
2025-01-16 16:22:36 -0700

    Override equals() method in ChessPosition.java

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -7,8 +7,12 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessPosition {
+    private int row;
+    private int col;
 
     public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
     /**
@@ -16,7 +20,7 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return this.row;
     }
 
     /**
@@ -24,6 +28,22 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return this.col;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof ChessPosition)) {
+            return false;
+        }
+        ChessPosition pos = (ChessPosition) o;
+        if (pos.row == this.row && pos.col == this.col) {
+            return true;
+        }
+
+        return false;
     }
 }
