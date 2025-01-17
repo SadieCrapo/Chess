9076c31b5483c51cd826bf2f7ce42d42857284d8
SadieCrapo <scrapo5@byu.edu>
2025-01-16 23:42:04 -0700

    Implement constructor, getTeamColor(), getPieceType(), and pieceMoves() in ChessPiece.java

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -10,7 +11,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
-    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+    private ChessGame.TeamColor pieceColor;
+    private PieceType type;
+
+    public ChessPiece(ChessGame.TeamColor pieceColor, PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -29,24 +35,58 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return this.pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return this.type;
     }
 
     /**
-     * Calculates all the positions a chess piece can move to
+     * Calculates all the positions a chess piece can move to.
      * Does not take into account moves that are illegal due to leaving the king in
      * danger
      *
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        switch (this.type) {
+            case KING -> {
+                return PieceMovesCalculator.pieceMoves(board, myPosition);
+            }
+            case QUEEN -> {
+                return PieceMovesCalculator.pieceMoves(board, myPosition);
+            }
+            case BISHOP -> {
+                return PieceMovesCalculator.pieceMoves(board, myPosition);
+            }
+            case KNIGHT -> {
+                return PieceMovesCalculator.pieceMoves(board, myPosition);
+            }
+            case ROOK -> {
+                return PieceMovesCalculator.pieceMoves(board, myPosition);
+            }
+            case PAWN -> {
+                return PieceMovesCalculator.pieceMoves(board, myPosition);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 }
