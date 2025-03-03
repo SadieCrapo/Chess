3bdeea928f257c0cecd908c5fce2c0489f141711
SadieCrapo <scrapo5@byu.edu>
2025-03-03 13:12:43 -0700

    Implement MemoryUserDAO.java

---
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -0,0 +1,33 @@
+package dataaccess;
+
+import model.UserData;
+
+import java.util.HashMap;
+
+public class MemoryUserDAO implements UserDAO {
+    private HashMap<String, UserData> userDatabase;
+
+    public MemoryUserDAO() {
+        userDatabase = new HashMap<>();
+    }
+
+    @Override
+    public void clear() {
+        userDatabase.clear();
+    }
+
+    @Override
+    public void createUser(UserData user) {
+        userDatabase.put(user.name, user);
+    }
+
+    @Override
+    public UserData getUser(String username) {
+        return userDatabase.get(username);
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        userDatabase.remove(username);
+    }
+}
