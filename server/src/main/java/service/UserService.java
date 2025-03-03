cc65b732eddf0c3ba3debae9bb2c3c85bf04fdbf
SadieCrapo <scrapo5@byu.edu>
2025-03-03 14:39:29 -0700

    Implement login() method in UserService.java

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,39 @@
+package service;
+
+import dataaccess.MemoryUserDAO;
+import model.AuthData;
+import model.UserData;
+import request.LoginRequest;
+import request.RegisterRequest;
+import server.Server;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class UserService {
+
+//    public RegisterResult register(RegisterRequest request) {
+//
+//    }
+
+    public LoginResult login(LoginRequest request) throws UnauthorizedException {
+        String username = request.username();
+        UserData user = Server.userDAO.getUser(username);
+        if (!Objects.equals(user.password(), request.password())) {
+            throw new UnauthorizedException("Password does not match");
+        }
+
+        String token = createAuthToken();
+        Server.authDAO.createAuth(new AuthData(token, username));
+
+        return new LoginResult(username, token);
+    }
+
+//    public void logout(LogoutRequest request) {
+//
+//    }
+
+    private String createAuthToken() {
+        return UUID.randomUUID().toString();
+    }
+}
