4b5b3b7133fa5b4d4d03231dc532bb84d8bbf62a
SadieCrapo <scrapo5@byu.edu>
2025-03-03 20:15:57 -0700

    Implement register() service

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -11,9 +11,20 @@ import java.util.UUID;
 
 public class UserService {
 
-//    public RegisterResult register(RegisterRequest request) {
-//
-//    }
+    public static RegisterResult register(RegisterRequest request) throws UsernameTakenException {
+        String username = request.username();
+        UserData user = new UserData(username, request.password(), request.email());
+        if (Server.userDAO.getUser(username) != null) {
+            throw new UsernameTakenException("Username has already been claimed");
+        }
+
+        Server.userDAO.createUser(user);
+
+        String token = createAuthToken();
+        Server.authDAO.createAuth(new AuthData(token, username));
+
+        return new RegisterResult(username, token);
+    }
 
     public static LoginResult login(LoginRequest request) throws UnauthorizedException {
         String username = request.username();
