7840bf1211881d7aaa83836fe950fee8a5310ef1
SadieCrapo <scrapo5@byu.edu>
2025-03-04 20:20:11 -0700

    Refactor ErrorHandler.java

--- server/src/main/java/handler/ErrorHandler.java
+++ server/src/main/java/handler/ErrorHandler.java
@@ -1,7 +1,6 @@
 package handler;
 
 import com.google.gson.Gson;
-import service.UnauthorizedException;
 import spark.Request;
 import spark.Response;
 
@@ -9,51 +8,30 @@ import java.util.Map;
 
 public class ErrorHandler {
     public Object unauthorizedHandler(Exception e, Request req, Response res) {
-        res.type("application/json");
-        res.status(401);
-        var body = new Gson().toJson(Map.of("message", "Error: unauthorized"));
-        res.body(body);
-        return body;
+        return errorHelper(res, 401, "Error: unauthorized");
     }
 
     public Object usernameTakenHandler(Exception e, Request req, Response res) {
-        res.type("application/json");
-        res.status(403);
-        var body = new Gson().toJson(Map.of("message", "Error: already taken"));
-        res.body(body);
-        return body;
+        return errorHelper(res, 403, "Error: already taken");
     }
 
     public Object badRequestHandler(Exception e, Request req, Response res) {
-        res.type("application/json");
-        res.status(400);
-        var body = new Gson().toJson(Map.of("message", "Error: bad request"));
-        res.body(body);
-        return body;
+        return errorHelper(res, 400, "Error: bad request");
     }
 
     public Object notFoundHandler(Request req, Response res) {
-            String msg = String.format("[%s] %s not found", req.requestMethod(), req.pathInfo());
-            var body = new Gson().toJson(Map.of("message", msg));
-            res.type("application/json");
-            res.status(400);
-            res.body(body);
-            return body;
+        return errorHelper(res, 400, String.format("[%s] %s not found", req.requestMethod(), req.pathInfo()));
     }
 
     public Object errorHandler(Exception e, Request req, Response res) {
-        var body = new Gson().toJson(Map.of("message", String.format("Error: %s", e.getMessage()), "success", false));
-        res.type("application/json");
-        res.status(500);
-        res.body(body);
-        return body;
+        return errorHelper(res, 500, String.format("Error: %s", e.getMessage()));
     }
 
-    protected static <T> T getBody(Request req, Class<T> classType) {
-        var body = new Gson().fromJson(req.body(), classType);
-        if (body == null) {
-            throw new RuntimeException("missing required body");
-        }
+    public Object errorHelper(Response res, int status, String msg) {
+        res.type("application/json");
+        res.status(status);
+        var body = new Gson().toJson(Map.of("message", msg, "success", false));
+        res.body(body);
         return body;
     }
 }
