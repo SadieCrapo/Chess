dabd9426b0aff64617e1927a3ad1cad79afb51f4
SadieCrapo <scrapo5@byu.edu>
2025-03-26 12:56:21 -0600

    Create tests for register() method

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,31 @@
 package client;
 
+import dataaccess.DataAccessException;
+import exception.BadRequestException;
 import org.junit.jupiter.api.*;
+import request.RegisterRequest;
+import result.RegisterResult;
 import server.Server;
-
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
+    private RegisterRequest newRegisterRequest = new RegisterRequest("newUser", "newUser", "newUser");
+    private RegisterRequest badRegisterRequest = new RegisterRequest("badUser", "badUser", null);
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade("http://localhost:" + port);
+    }
+
+    @BeforeEach
+    public void setUp() throws BadRequestException, DataAccessException {
+        tearDown();
+        facade.register(new RegisterRequest("oldUser", "oldUser", "oldUser"));
     }
 
     @AfterAll
@@ -20,10 +33,42 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @Test
+    @DisplayName("Successfully register new user")
+    public void successRegister() throws BadRequestException {
+        RegisterResult result = facade.register(newRegisterRequest);
+        Assertions.assertEquals("newUser", result.username());
+        Assertions.assertNotEquals("", result.authToken());
+    }
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    @DisplayName("Fail to register new user because no email")
+    public void failRegisterNoEmail() {
+        String message = "";
+        try {
+            RegisterResult result = facade.register(badRegisterRequest);
+        } catch (BadRequestException e) {
+            message = e.getMessage();
+        }
+        Assertions.assertEquals("Error: bad request", message);
+    }
+
+    @Test
+    @DisplayName("Fail to register new user because already exists")
+    public void failRegisterAlreadyExists() {
+        String message = "";
+        try {
+            RegisterResult result = facade.register(new RegisterRequest("oldUser", "oldUser", "oldUser"));
+        } catch (BadRequestException e) {
+            message = e.getMessage();
+        }
+        Assertions.assertEquals("Error: already taken", message);
+    }
+
+    public void tearDown() throws DataAccessException {
+        server.userDAO.clear();
+        server.authDAO.clear();
+        server.gameDAO.clear();
     }
 
 }
