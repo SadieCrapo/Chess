2513acfd79ef4fc09764d682546ee28f4dfafc11
SadieCrapo <scrapo5@byu.edu>
2025-03-26 13:43:37 -0600

    Create PostLoginClient.java

---
+++ client/src/main/java/client/PostLoginClient.java
@@ -0,0 +1,76 @@
+package client;
+
+import exception.BadRequestException;
+import repl.REPL;
+
+import java.util.Arrays;
+
+public class PostLoginClient implements Client {
+    private final String serverUrl;
+    private ServerFacade server;
+    final REPL repl;
+
+    public PostLoginClient(String serverUrl, ServerFacade server, REPL repl) {
+        this.serverUrl = serverUrl;
+        this.server = server;
+        this.repl = repl;
+    }
+
+    @Override
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "logout" -> logout();
+                case "create" -> create(params);
+                case "list" -> list();
+                case "join" -> join(params);
+                case "observe" -> observe(params);
+                case "quit" -> quit();
+                default -> help();
+            };
+//        } catch (BadRequestException ex) {
+//            return ex.getMessage();
+        } catch (Exception ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public String logout() {
+        return "";
+    }
+
+    public String create(String... params) {
+        return "";
+    }
+
+    public String list() {
+        return "";
+    }
+
+    public String join(String... params) {
+        return "";
+    }
+
+    public String observe(String... params) {
+        return "";
+    }
+
+    public String quit(String... params) {
+        logout();
+        return "quit";
+    }
+
+    @Override
+    public String help() {
+        return """
+                Options:
+                create <name> - create a new game
+                list - view current games
+                join <ID> <black/white> - play chess as specified color
+                observe <ID> - watch specified game
+                logout - to return to start
+                quit - logout and exit chess""";    }
+}
