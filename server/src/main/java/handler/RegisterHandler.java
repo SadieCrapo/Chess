51fa915b8f7d7c1d67595424627432aba8dff5f9
SadieCrapo <scrapo5@byu.edu>
2025-03-30 19:20:39 -0600

    Remove redundant try block

--- server/src/main/java/handler/RegisterHandler.java
+++ server/src/main/java/handler/RegisterHandler.java
@@ -14,14 +14,9 @@ import exception.BadRequestException;
 public class RegisterHandler extends Handler {
     @Override
     public Object handleRequest(Request req, Response res) throws UsernameTakenException, BadRequestException, DataAccessException {
-        RegisterResult result;
 
         RegisterRequest request = getBody(req, RegisterRequest.class);
-        try {
-            result = UserService.register(request);
-        } catch (UsernameTakenException e) {
-            throw e;
-        }
+        RegisterResult result = UserService.register(request);
 
         res.type("application/json");
         return new Gson().toJson(result);
