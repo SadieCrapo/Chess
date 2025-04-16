4723f66e33ac9771fb928070f3a0b720cd222b2b
SadieCrapo <scrapo5@byu.edu>
2025-04-15 23:02:42 -0600

    Implement makeMove

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,15 +1,18 @@
 package server.websocket;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
 
 import websocket.messages.ServerMessage;
 
 public class ConnectionManager {
     public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+    private static final Logger logger = Logger.getLogger("ConnectionManager");
 
     public void add(String username, Session session) {
         var connection = new Connection(username, session);
@@ -39,19 +42,11 @@ public class ConnectionManager {
     }
 
     public void send(String username, ServerMessage message) throws IOException {
-        var removeList = new ArrayList<Connection>();
-        for (var c : connections.values()) {
-            if (c.session.isOpen()) {
-                if (c.username.equals(username)) {
-                    c.send(message.toString());
-                }
-            } else {
-                removeList.add(c);
-            }
-        }
-
-        for (var c : removeList) {
-            connections.remove(c.username);
+        Connection connection = connections.get(username);
+        if (connection != null && connection.session.isOpen()) {
+            connection.send(new Gson().toJson(message));
+        } else {
+            connections.remove(username);
         }
     }
 }
--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,5 +1,9 @@
 package server.websocket;
 
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessMove;
+import chess.InvalidMoveException;
 import com.google.gson.Gson;
 import dataaccess.DataAccessException;
 import exception.UnauthorizedException;
@@ -16,20 +20,20 @@ import websocket.commands.UserGameCommand;
 import websocket.messages.NotificationMessage;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @WebSocket
 public class WebSocketHandler {
     private final ConnectionManager connections = new ConnectionManager();
+    private static final Logger logger = Logger.getLogger("WebSocketHandler");
+
 
     @OnWebSocketMessage
     public void onMessage(Session session, String message) throws DataAccessException, UnauthorizedException, IOException {
-//        if (message.contains("CONNECT")) {
-//            connect("authToken", session);
-//        }
         UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
         switch (command.getCommandType()) {
             case CONNECT -> connect(command.getAuthToken(), command.getGameID(), session);
-            case MAKE_MOVE -> makeMove();
+            case MAKE_MOVE -> makeMove(command.getAuthToken(), command.getGameID(), command.getMove(), session);
             case RESIGN -> resign();
             case LEAVE -> leave();
         }
@@ -55,15 +59,47 @@ public class WebSocketHandler {
         }
     }
 
-//    private void enter(String visitorName, Session session) throws IOException {
-//        connections.add(visitorName, session);
-//        var message = String.format("%s is in the shop", visitorName);
-//        var notification = new Notification(Notification.Type.ARRIVAL, message);
-//        connections.broadcast(visitorName, notification);
-//    }
+    private void makeMove(String authToken, int gameID, ChessMove move, Session session) throws DataAccessException, UnauthorizedException, IOException {
+        String username = authToken;
+        try {
+            username = Server.authDAO.getAuth(authToken).username();
 
-    private void makeMove() {
+            GameData gameData = Server.gameDAO.getGame(gameID);
+            ChessGame game = gameData.game();
 
+            ChessGame.TeamColor currentTeam = game.getTeamTurn();
+            if (currentTeam == ChessGame.TeamColor.BLACK) {
+                if (!username.equals(gameData.blackUsername())) {
+                    var errorMessage = new ErrorMessage("Wrong team");
+                    connections.send(username, errorMessage);
+                    return;
+                }
+            } else if (!username.equals(gameData.whiteUsername())) {
+                var errorMessage = new ErrorMessage("Wrong team");
+                connections.send(username, errorMessage);
+                return;
+            }
+
+            game.makeMove(move);
+            GameData updatedGameData = new GameData(gameID, gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName(), game);
+            logger.info(new Gson().toJson(Server.gameDAO.getGame(gameID).game()));
+            Server.gameDAO.updateGame(updatedGameData);
+            logger.info(new Gson().toJson(Server.gameDAO.getGame(gameID).game()));
+
+            var message = String.format("%s made move %s", username, move);
+            var notification = new NotificationMessage(message);
+            var loadMessage = new LoadGameMessage(game);
+            connections.broadcast(username, notification);
+            connections.broadcast("", loadMessage);
+        } catch (NullPointerException e) {
+            var errorMessage = new ErrorMessage(e.getMessage());
+            connections.add(username, session);
+            connections.send(username, errorMessage);
+            connections.remove(username);
+        } catch (InvalidMoveException e) {
+            var errorMessage = new ErrorMessage(e.getMessage());
+            connections.send(username, errorMessage);
+        }
     }
 
     private void resign() {
