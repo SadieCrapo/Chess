4eed2f2facef728d3418f57994cb43137c5a4b9c
SadieCrapo <scrapo5@byu.edu>
2025-04-16 18:38:50 -0600

    Remove logger

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,13 +1,10 @@
 package server.websocket;
 
-import chess.ChessBoard;
-import chess.ChessGame;
-import chess.ChessMove;
-import chess.InvalidMoveException;
+import chess.*;
 import com.google.gson.Gson;
 import dataaccess.DataAccessException;
+import exception.BadRequestException;
 import exception.UnauthorizedException;
-import model.AuthData;
 import model.GameData;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
@@ -26,8 +23,6 @@ import java.util.logging.Logger;
 @WebSocket
 public class WebSocketHandler {
     private final ConnectionManager connections = new ConnectionManager();
-    private static final Logger logger = Logger.getLogger("WebSocketHandler");
-
 
     @OnWebSocketMessage
     public void onMessage(Session session, String message) throws DataAccessException, UnauthorizedException, IOException {
@@ -53,7 +48,7 @@ public class WebSocketHandler {
             var loadMessage = new LoadGameMessage(gameData.game());
             connections.send(username, loadMessage);
         } catch (NullPointerException e) {
-            var errorMessage = new ErrorMessage(e.getMessage());
+            var errorMessage = new ErrorMessage("Error: " + e.getMessage());
             connections.add(username, session, gameID);
             connections.send(username, errorMessage);
             connections.remove(username);
@@ -68,15 +63,21 @@ public class WebSocketHandler {
             GameData gameData = Server.gameDAO.getGame(gameID);
             ChessGame game = gameData.game();
 
+            if (game.getBoard() == new ChessBoard()) {
+                var errorMessage = new ErrorMessage("Error: can't move after player resigns");
+                connections.send(username, errorMessage);
+                return;
+            }
+
             ChessGame.TeamColor currentTeam = game.getTeamTurn();
             if (currentTeam == ChessGame.TeamColor.BLACK) {
                 if (!username.equals(gameData.blackUsername())) {
-                    var errorMessage = new ErrorMessage("Wrong team");
+                    var errorMessage = new ErrorMessage("Error: not your turn");
                     connections.send(username, errorMessage);
                     return;
                 }
             } else if (!username.equals(gameData.whiteUsername())) {
-                var errorMessage = new ErrorMessage("Wrong team");
+                var errorMessage = new ErrorMessage("Error: not your turn");
                 connections.send(username, errorMessage);
                 return;
             }
@@ -85,20 +86,85 @@ public class WebSocketHandler {
             GameData updatedGameData = new GameData(gameID, gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName(), game);
             Server.gameDAO.updateGame(updatedGameData);
 
-            var message = String.format("%s made move %s", username, move);
+            String parsedMove = parseMove(move);
+
+            var message = String.format("%s made move %s", username, parsedMove);
             var notification = new NotificationMessage(message);
             var loadMessage = new LoadGameMessage(game);
-            connections.broadcast(username, notification, gameID);
             connections.broadcast("", loadMessage, gameID);
+            connections.broadcast(username, notification, gameID);
+
+            if (game.isInCheckmate(ChessGame.TeamColor.WHITE)) {
+                var checkmateMessage = String.format("Game over, %s is in checkmate", gameData.whiteUsername());
+                connections.broadcast("", new NotificationMessage(checkmateMessage), gameID);
+                return;
+            } else if (game.isInCheck(ChessGame.TeamColor.WHITE)) {
+                var checkMessage = String.format("%s is in check", gameData.whiteUsername());
+                connections.broadcast("", new NotificationMessage(checkMessage), gameID);
+            }
+            if (game.isInCheckmate(ChessGame.TeamColor.BLACK)) {
+                var checkmateMessage = String.format("Game over, %s is in checkmate", gameData.blackUsername());
+                connections.broadcast("", new NotificationMessage(checkmateMessage), gameID);
+                return;
+            } else if (game.isInCheck(ChessGame.TeamColor.BLACK)) {
+                var checkMessage = String.format("%s is in check", gameData.whiteUsername());
+                connections.broadcast("", new NotificationMessage(checkMessage), gameID);
+            }
+            if (game.isInStalemate(ChessGame.TeamColor.BLACK)) {
+                var stalemateMessage = String.format("Game over, %s is in stalemate", gameData.blackUsername());
+                connections.broadcast("", new NotificationMessage(stalemateMessage), gameID);
+            } else if (game.isInStalemate(ChessGame.TeamColor.WHITE)) {
+                var stalemateMessage = String.format("Game over, %s is in stalemate", gameData.blackUsername());
+                connections.broadcast("", new NotificationMessage(stalemateMessage), gameID);
+            }
         } catch (NullPointerException e) {
-            var errorMessage = new ErrorMessage(e.getMessage());
+            var errorMessage = new ErrorMessage("Error: " + e.getMessage());
             connections.add(username, session, gameID);
             connections.send(username, errorMessage);
             connections.remove(username);
-        } catch (InvalidMoveException e) {
-            var errorMessage = new ErrorMessage(e.getMessage());
+        } catch (InvalidMoveException | BadRequestException e) {
+            var errorMessage = new ErrorMessage("Error: " + e.getMessage());
             connections.send(username, errorMessage);
         }
+    }
+
+    private String parseMove(ChessMove move) throws BadRequestException {
+        ChessPosition start = move.getStartPosition();
+        ChessPosition end = move.getEndPosition();
+
+        int startRow = start.getRow();
+        int startCol = start.getColumn();
+        int endRow = end.getRow();
+        int endCol = end.getColumn();
+
+        String stringStartCol;
+        String stringEndCol;
+
+        stringStartCol = switch (startCol) {
+            case 1 -> "a";
+            case 2 -> "b";
+            case 3 -> "c";
+            case 4 -> "d";
+            case 5 -> "e";
+            case 6 -> "f";
+            case 7 -> "g";
+            case 8 -> "h";
+            default -> throw new BadRequestException("col must be integer from 1-8");
+        };
+        stringEndCol = switch (endCol) {
+            case 1 -> "a";
+            case 2 -> "b";
+            case 3 -> "c";
+            case 4 -> "d";
+            case 5 -> "e";
+            case 6 -> "f";
+            case 7 -> "g";
+            case 8 -> "h";
+            default -> throw new BadRequestException("col must be integer from 1-8");
+        };
+        String result = stringStartCol + String.valueOf(startRow) + " to " + stringEndCol + String.valueOf(endRow);
+
+        return result;
     }
 
     private void resign(String authToken, int gameID, Session session) throws DataAccessException, UnauthorizedException, IOException {
@@ -109,13 +175,13 @@ public class WebSocketHandler {
             ChessGame game = gameData.game();
 
             if (!(username.equals(gameData.blackUsername()) || username.equals(gameData.whiteUsername()))) {
-                var errorMessage = new ErrorMessage("observer cannot resign");
+                var errorMessage = new ErrorMessage("Error: observer cannot resign");
                 connections.send(username, errorMessage);
                 return;
             }
 
             if (Objects.equals(game.getBoard(), new ChessBoard())) {
-                var errorMessage = new ErrorMessage("cannot resign after opponent");
+                var errorMessage = new ErrorMessage("Error: cannot resign after opponent");
                 connections.send(username, errorMessage);
                 return;
             }
@@ -129,7 +195,7 @@ public class WebSocketHandler {
             var notification = new NotificationMessage(message);
             connections.broadcast("", notification, gameID);
         } catch (NullPointerException e) {
-            var errorMessage = new ErrorMessage(e.getMessage());
+            var errorMessage = new ErrorMessage("Error: " + e.getMessage());
             connections.add(username, session, gameID);
             connections.send(username, errorMessage);
             connections.remove(username);
@@ -157,7 +223,7 @@ public class WebSocketHandler {
             var notification = new NotificationMessage(message);
             connections.broadcast(username, notification, gameID);
         } catch (NullPointerException e) {
-            var errorMessage = new ErrorMessage(e.getMessage());
+            var errorMessage = new ErrorMessage("Error: " + e.getMessage());
             connections.add(username, session, gameID);
             connections.send(username, errorMessage);
             connections.remove(username);
